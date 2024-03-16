3ae2f47f33ae76709ea4f0402f92507118976d1e
anjie_f <amfarmer2022@gmail.com>
2024-03-15 19:55:40 -0500

    Written 3 params

---
+++ script.js
@@ -0,0 +1,21 @@
+//write a function
+var startMyDay = function () {
+    return (`Time for fresh ground coffee and get Phil'd in!`);
+};
+startMyDay();
+
+//write a function with 1 parameter
+var favoriteCookie = function (cookie) {
+    console.log(`My favorite cookie is ${cookie}`);
+}
+favoriteCookie("Oatmeal Raisin");
+
+//write a function with 2 parameter
+var introduce = function (name, occupation) {
+    console.log(`My name is ${name} and by May 2024 I will be a ${occupation}!`);
+};
+
+introduce("Anjie","front-end developer");
+introduce("Raymond", "a simple businessman");
+
+//create a function to capture input
